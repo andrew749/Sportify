4bfdeb5703c9ccb1e1500d6f6c6562da0520f752
Andrew Codispoti <andrewcod749@gmail.com>
2015-06-15 20:52:04 -0400

    Updated sport view controller to get time and handle zeros.

--- Sportify/SportViewController.swift
+++ Sportify/SportViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class SportViewController:UIViewController{
+    @IBOutlet weak var awayScore: UILabel!
+    @IBOutlet weak var homeScore: UILabel!
     var sport:SPORT_TYPE?;
     var sportName:String?
+    @IBOutlet weak var timeLabel: UILabel!
     var pointLimit:Int?
-    var timeLimit:NSDate?
+    var timeLimit:(Int,Int,Int)=(0,0,0)
     override func viewDidLoad() {
         self.title=sportName
+        var timeLimitString:(String,String,String)
+        if(timeLimit.0<10){
+            timeLimitString.0="0\(timeLimit.0)"
+        }else{
+            timeLimitString.0="\(timeLimit.0)"
+        }
+        if(timeLimit.1<10){
+            timeLimitString.1="0\(timeLimit.1)"
+        }else{
+            timeLimitString.1="\(timeLimit.1)"
+        }
+        if(timeLimit.2<10){
+            timeLimitString.2="0\(timeLimit.2)"
+        }else{
+            timeLimitString.2="\(timeLimit.2)"
+        }
+        self.timeLabel.text="\(timeLimitString.0):\(timeLimitString.1):\(timeLimitString.2)"
+
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
--- Sportify/SportsOptionPicker.swift
+++ Sportify/SportsOptionPicker.swift
@@ -11,24 +11,32 @@ import UIKit
 class SportsOptionPicker:UIViewController,UIPickerViewDataSource,UIPickerViewDelegate{
     var sportName:String?
     var sport:SPORT_TYPE?
-    var timerView:UIDatePicker?
-    var pointView:UIPickerView?
+    var timerView:TimePickerView
+    var pointView:UIPickerView
     var pickerData=[Int]()
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        timerView=TimePickerView()
+        pointView=UIPickerView()
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        pointView.delegate=self
+
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        timerView=TimePickerView()
+        pointView=UIPickerView()
+        super.init(coder: aDecoder)
+        pointView.delegate=self
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title=sportName
         for i in 1...200{
             self.pickerData.append(i)
         }
-        timerView=UIDatePicker()
-        timerView!.datePickerMode=UIDatePickerMode.CountDownTimer
-        
-        pointView=UIPickerView()
-        pointView!.delegate=self
-        
-        self.optionsView.addSubview(pointView!)
-        pointView?.hidden=true
-        self.optionsView.addSubview(timerView!)
+        self.optionsView.addSubview(pointView)
+        pointView.hidden=true
+        self.optionsView.addSubview(timerView)
         segControl.addTarget(self, action: "toggleSelectionView", forControlEvents: UIControlEvents.ValueChanged)
     }
     
@@ -39,15 +47,15 @@ class SportsOptionPicker:UIViewController,UIPickerViewDataSource,UIPickerViewDel
         // Dispose of any resources that can be recreated.
     }
     func toggleSelectionView(){
-        if pointView!.hidden{
-            pointView?.hidden=false
-            timerView?.hidden=true
-            optionsView.bringSubviewToFront(timerView!)
+        if pointView.hidden{
+            pointView.hidden=false
+            timerView.hidden=true
+            optionsView.bringSubviewToFront(timerView)
         }
         else{
-            pointView?.hidden=true
-            timerView?.hidden=false;
-            optionsView.bringSubviewToFront(pointView!)
+            pointView.hidden=true
+            timerView.hidden=false;
+            optionsView.bringSubviewToFront(pointView)
         }
     }
 
@@ -66,11 +74,82 @@ class SportsOptionPicker:UIViewController,UIPickerViewDataSource,UIPickerViewDel
     }
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if(segue.identifier=="gamestart"){
-            if let svc=sender!.destinationViewController as? SportViewController{
+            if let svc=segue.destinationViewController as? SportViewController{
                 svc.sport==self.sport
                 svc.sportName=self.sportName
-                
+                svc.pointLimit=pickerData[pointView.selectedRowInComponent(0)]
+                svc.timeLimit=(timerView.hour,timerView.minute,timerView.second)
             }
         }
+    }
+    class TimePickerView: UIPickerView, UIPickerViewDataSource, UIPickerViewDelegate {
+        var hour:Int = 0
+        var minute:Int = 0
+        var second:Int = 0
+        
+        
+        required internal init(coder aDecoder: NSCoder) {
+            super.init(coder: aDecoder)
+            self.setup()
+        }
+        
+        override init(frame: CGRect) {
+            super.init(frame: frame)
+            self.setup()
+        }
+        
+        func setup(){
+            self.delegate = self
+            self.dataSource = self
+        }
+        
+        func getDate() -> NSDate{
+            let dateFormatter = NSDateFormatter()
+            dateFormatter.dateFormat = "HH:mm:ss"
+            let date = dateFormatter.dateFromString(String(format: "%02d", self.hour) + ":" + String(format: "%02d", self.minute))
+            return date!
+        }
+        
+        func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+            return 3
+        }
+        
+        func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+            switch component {
+            case 0:
+                self.hour = row
+            case 1:
+                self.minute = row
+            case 2:
+                self.second = row
+            default:
+                println("No component with number \(component)")
+            }
+        }
+        
+        func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+            if component == 0 {
+                return 24
+            }else if component == 1{
+                return 60
+            }
+            return 60
+        }
+        
+        func pickerView(pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+            return 30
+        }
+        func pickerView(pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusingView view: UIView!) -> UIView {
+            if (view != nil) {
+                (view as! UILabel).text = String(format:"%02lu", row)
+                return view
+            }
+            let columnView = UILabel(frame: CGRectMake(35, 0, self.frame.size.width/3 - 35, 30))
+            columnView.text = String(format:"%02lu", row)
+            columnView.textAlignment = NSTextAlignment.Center
+            
+            return columnView
+        }
+        
     }
 }
